fb74f50de672dfdbef5cc3b6957c4ad8553c430f
FlorentDB <florent.donat.bouillud@gmail.com>
2025-08-25 14:32:24 +0200

    rajout build_interval_table

--- markov.py
+++ markov.py
@@ -123,6 +123,19 @@ def build_interval_dict(chord_map):
         interval_dict[chord] = intervals
     return interval_dict
 
+def build_interval_table(data, max_order):
+    table = defaultdict(lambda: defaultdict(int))
+    for seq in data.values():
+        seq = list(seq)
+        for i in range(len(seq)):
+            succ = seq[i]
+            for order in range(1, max_order + 1):
+                if i - order < 0:
+                    break
+                ctx = tuple(seq[i-order:i])
+                table[ctx][succ] += 1
+    return {ctx: dict(d) for ctx, d in table.items()}
+
 def generate_symbol_vlmc(
     previous_symbols: List[Any],
     vlmc_table: Dict[Tuple, Dict[Tuple, int]],
